f5a9c1bf1023c05f0c95f4be97be6f722f4f80f5
Amy Haywood Dutton <me@amyhaywood.com>
2023-11-01 11:56:44 -0500

    ✨ Built the Icon component

---
+++ web/src/components/Icon/Icon.stories.tsx
@@ -0,0 +1,77 @@
+// Pass props to your component by passing an `args` object to your story
+//
+// ```tsx
+// export const Primary: Story = {
+//  args: {
+//    propName: propValue
+//  }
+// }
+// ```
+//
+// See https://storybook.js.org/docs/react/writing-stories/args.
+
+import type { Meta, StoryObj } from '@storybook/react'
+
+import Icon from './Icon'
+
+const meta: Meta<typeof Icon> = {
+  component: Icon,
+}
+
+export default meta
+
+type Story = StoryObj<typeof Icon>
+
+export const Arrow: Story = {
+  args: {
+    id: 'arrow',
+  },
+}
+
+export const Facebook: Story = {
+  args: {
+    id: 'facebook',
+  },
+}
+
+export const GitHub: Story = {
+  args: {
+    id: 'github',
+  },
+}
+
+export const hide: Story = {
+  args: {
+    id: 'hide',
+  },
+}
+
+export const Linkedin: Story = {
+  args: {
+    id: 'linkedin',
+  },
+}
+
+export const Show: Story = {
+  args: {
+    id: 'show',
+  },
+}
+
+export const Twitter: Story = {
+  args: {
+    id: 'twitter',
+  },
+}
+
+export const Up: Story = {
+  args: {
+    id: 'up',
+  },
+}
+
+export const Youtube: Story = {
+  args: {
+    id: 'youtube',
+  },
+}
