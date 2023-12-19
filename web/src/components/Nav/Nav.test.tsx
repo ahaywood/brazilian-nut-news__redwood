477002fa74a80849ae4a27ee9f9b7a97f32d436d
Amy Haywood Dutton <me@amyhaywood.com>
2023-12-18 23:08:28 -0600

    ✅ Wrote some tests for the nav component

--- web/src/components/Nav/Nav.test.tsx
+++ web/src/components/Nav/Nav.test.tsx
@@ -1,4 +1,5 @@
-import { render } from '@redwoodjs/testing/web'
+import { routes } from '@redwoodjs/router'
+import { render, screen } from '@redwoodjs/testing/web'
 
 import Nav from './Nav'
 
@@ -10,5 +11,45 @@ describe('Nav', () => {
     expect(() => {
       render(<Nav />)
     }).not.toThrow()
+  })
+
+  it('has a link to the home page', () => {
+    render(<Nav />)
+    expect(screen.getByText('Home')).toHaveAttribute('href', routes.feed())
+  })
+
+  describe('when a user is not logged in', () => {
+    it('shows a link to sign up', () => {
+      render(<Nav />)
+      expect(screen.getByText('Sign Up')).toBeInTheDocument()
+    })
+
+    it('shows a link to login', () => {
+      render(<Nav />)
+      expect(screen.getByText('Login')).toBeInTheDocument()
+    })
+  })
+
+  describe('when a user is logged in', () => {
+    beforeEach(() => {
+      mockCurrentUser({ id: 1, nickname: 'amy' })
+    })
+
+    it('shows a Submit a Link button', async () => {
+      render(<Nav />)
+      expect(await screen.findByText('Submit a Link')).toBeInTheDocument()
+    })
+
+    it('shows a Logout button', async () => {
+      // mockCurrentUser({ id: 1, nickname: 'amy' })
+      render(<Nav />)
+      expect(await screen.findByText('Logout')).toBeInTheDocument()
+    })
+
+    it('shows a My Profile button', async () => {
+      // mockCurrentUser({ id: 1, nickname: 'amy' })
+      render(<Nav />)
+      expect(await screen.findByText('My Profile')).toBeInTheDocument()
+    })
   })
 })
